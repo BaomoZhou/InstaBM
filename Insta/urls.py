de1707eca1616f9f97f5bcbded40160dba18e01d
Baomo Zhou <bazhou@eng.ucsd.edu>
2020-04-08 18:18:08 -0700

    nothing

--- Insta/urls.py
+++ Insta/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='make_post'),
     path('post/update/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
     path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
-    path('like', addLike, name='addLike'),
+    path('like/', addLike, name='addLike'),
     path('user/<int:pk>/', UserDetailView.as_view(), name='user_detail')
 ]
